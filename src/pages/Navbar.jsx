95899e7d9f37af997e171310811adef8dbe452f3
Aren <aerenevli@gmail.com>
2024-12-01 09:22:01 +0300

    Navbar icon

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { GiShoppingBag } from "react-icons/gi";
 
 const Navbar = () => {
   return (
@@ -13,6 +14,10 @@ const Navbar = () => {
             <Link to="/">All Products</Link>
             <Link to="/men">Men's Clothing</Link>
             <Link to="/women">Women's Clothing</Link>
+        </div>
+        <div className='flex justify-center items-center bg-white rounded-full cursor-pointer relative'>
+          <GiShoppingBag className='w-8 h-8 text-gray-900 p-1' />
+          <span className='flex justify-center items-center absolute top-2/3 -left-1  bg-red-600 w-4 h-4 text-white rounded-full '>0</span>
         </div>
     </div>
   )
